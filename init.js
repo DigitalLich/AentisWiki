b5e2c7ed5b0620b13066d4631cd16750b2b0e975
Edwin <61763285+DigitalLich@users.noreply.github.com>
2025-10-17 18:09:20 -0700

    deleting init

--- init.js
+++
@@ -1,6 +0,0 @@
-const isLocal = location.hostname === "127.0.0.1" || location.hostname === "localhost";
-    if (isLocal) {
-    document.write('<base href="/">');
-    } else {
-    document.write('<base href="/AentisWiki/">');
-    }
